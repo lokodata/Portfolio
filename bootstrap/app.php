31b7e8aede9c7d509a4f1ccdddf23c7b126040c6
lokodata <lokodata115@gmail.com>
2025-04-03 17:59:57 +0800

    fix https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\TrustProxies;
+use App\Http\Middleware\HttpsProtocol;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(TrustProxies::class);
+        $middleware->append(HttpsProtocol::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
